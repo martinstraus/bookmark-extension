9be8793c1fcddf4f5680927fff25de6090f9de57
Martin Straus <martinstraus@gmail.com>
2024-01-14 11:35:57 -0300

    Remove default popup and add action to toggle extension text.

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Levadura's Bookmark installed.");
 });
+
+chrome.action.onClicked.addListener(async (tab) => {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState !== '+' ? '+' : ''
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+});
